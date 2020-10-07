29e9bf5cdc44670b021dea4cf5c99c522d32577d
Willy dongmo <w.dongmo@gmail.com>
2020-10-07 11:41:38 +0200

    Last minute payment

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -44,7 +44,7 @@ const start = async () => {
     console.log(err);
   }
   app.listen(3000, () => {
-    console.log('listening on port 3000 !');
+    console.log('Payment listening on port 3000 !');
   });
 };
 
